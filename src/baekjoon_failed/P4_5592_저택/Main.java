9a6d338df7c955402b5abd96b6edd9245cbd4c2f
OiPKL <dhkswls1998@naver.com>
2025-03-12 23:02:27 +0900

    250312

--- src/baekjoon_failed/P4_5592_저택/Main.java
+++ src/baekjoon_failed/P4_5592_저택/Main.java
@@ -3,10 +3,7 @@ package baekjoon_failed.P4_5592_저택;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -20,36 +17,43 @@ public class Main {
 
         boolean[][][] visited = new boolean[N][M][2];
 
-        @SuppressWarnings("unchecked")
-        List<Integer>[] listN = new ArrayList[N];                // axis 1
-        @SuppressWarnings("unchecked")
-        List<Integer>[] listM = new ArrayList[M];                // axis 0
-
-        for (int n = 0; n < N; n++)
-            listN[n] = new ArrayList<>();
-        for (int m = 0; m < M; m++)
-            listM[m] = new ArrayList<>();
+        HashMap<Integer, List<Integer>> keys = new HashMap<>();
 
         for (int k = 0; k < K; k++) {
             st = new StringTokenizer(br.readLine());
             int btnM = Integer.parseInt(st.nextToken()) - 1;
             int btnN = Integer.parseInt(st.nextToken()) - 1;
 
-            listN[btnN].add(btnM);
-            listM[btnM].add(btnN);
+            int keyN = btnN;
+            if (!keys.containsKey(keyN))
+                keys.put(keyN, new ArrayList<>());
+            keys.get(keyN).add(btnM);
+
+            int keyM = 100000 + btnM;
+            if (!keys.containsKey(keyM))
+                keys.put(keyM, new ArrayList<>());
+            keys.get(keyM).add(btnN);
         }
 
         // 종료지점 스위치 CASE
-        if (!listN[N - 1].contains(M - 1)) {
-            listN[N - 1].add(M - 1);
-            listM[M - 1].add(N - 1);
+        if (!(keys.containsKey(N - 1) && keys.get(N - 1).contains(M - 1))) {
+
+            int keyN = N - 1;
+            if (!keys.containsKey(keyN))
+                keys.put(keyN, new ArrayList<>());
+            keys.get(keyN).add(M - 1);
+
+            int keyM = 100000 + (M - 1);
+            if (!keys.containsKey(keyM))
+                keys.put(keyM, new ArrayList<>());
+            keys.get(keyM).add(N - 1);
         }
 
         PriorityQueue<int[]> bfs = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         bfs.add(new int[]{0, 0, 0, 0});
 
         // 시작지점 스위치 CASE
-        if (listN[0].contains(0))
+        if (keys.containsKey(0) && keys.get(0).contains(0))
             bfs.add(new int[]{0, 0, 1, 1});
 
         while (!bfs.isEmpty()) {
@@ -76,24 +80,24 @@ public class Main {
 
             if (axis == 0) {
 
-                for (int nNext : listM[mNow]) {
-
-                    if (!visited[nNext][mNow][1]) {
-
-                        int tmp = time + Math.abs(nNext - nNow);
-                        if (bfs.size() < 100)
+                int keyM = 100000 + mNow;
+                if (keys.containsKey(keyM)) {
+                    for (int nNext : keys.get(keyM)) {
+                        if (!visited[nNext][mNow][1]) {
+                            int tmp = time + Math.abs(nNext - nNow);
                             bfs.add(new int[]{nNext, mNow, tmp + 1, 1});
+                        }
                     }
                 }
             } else {
 
-                for (int mNext : listN[nNow]) {
-
-                    if (!visited[nNow][mNext][1]) {
-
-                        int tmp = time + Math.abs(mNext - mNow);
-                        if (bfs.size() < 100)
-                            bfs.add(new int[]{nNow, mNext, tmp + 1, 0});
+                int keyN = nNow;
+                if (keys.containsKey(keyN)) {
+                    for (int mNext : keys.get(keyN)) {
+                        if (!visited[keyN][mNext][0]) {
+                            int tmp = time + Math.abs(mNext - mNow);
+                            bfs.add(new int[]{keyN, mNext, tmp + 1, 0});
+                        }
                     }
                 }
             }
