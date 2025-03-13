f27698dc9238847671907e2702d834b2f42a85f7
OiPKL <dhkswls1998@naver.com>
2025-03-13 17:51:04 +0900

    250313

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -5,19 +5,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
+	static int N, cnt = 0;
+	static int[] arr1, arr2;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
-		int[] before = new int[N];
-		int[] after = new int[N];
+		N = Integer.parseInt(br.readLine());
+		arr1 = new int[N];
+		arr2 = new int[N];
 
-		String line = br.readLine();
-		for (int i = 0; i < N; i++)
-			before[i] = line.charAt(i);
-		line = br.readLine();
-		for (int i = 0; i < N; i++)
-			after[i] = line.charAt(i);
+		String str1 = br.readLine();
+		String str2 = br.readLine();
+		for (int n = 0; n < N; n++) {
+
+			int num1 = str1.charAt(n);
+			int num2 = str2.charAt(n);
+
+			if (num1 != num2) {
+
+				
+			}
+		}
     }
 }
