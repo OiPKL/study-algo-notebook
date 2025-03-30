aeebcfb9a3c42a957b1398f88b2bcd1635f37a30
OiPKL <dhkswls1998@naver.com>
2025-03-30 22:19:52 +0900

    250330

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,79 +3,51 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int[] dn = {-1, 1, 0, 0};
-	static int[] dm = {0, 0, -1, 1};
-
-	static int N, M, K, time = -1;
-	static int[][] map, sharkNM;
-	static int[][][] sharkPQ;
-	static HashSet<Integer> sharkList;
-
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int[] numbers = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+		for (int n = 0; n < N; n++)
+			numbers[n] = Integer.parseInt(st.nextToken());
 
-		map = new int[N][N];
-		sharkNM = new int[M + 1][3];
-		sharkPQ = new int[M + 1][4][4];
-		sharkList = new HashSet<>();
+		Arrays.sort(numbers);
 
-		/*
-		map			냄새
-		sharkNM		상어 | n, m, d
-		sharkPQ		상어 | 방향 | 우선순위
-		sharkList	생존상어
-		sharkDelete	삭제상어
-		sharkCheck	삭제체크
-		 */
-		
-		// HashMap<mapKey, time>[M+1] 추가 필요
+		int cnt = 0;
+		for (int n = 0; n < N; n++) {
+			int T = numbers[n];
+			int L = 0;
+			int R = N - 1;
 
-		for (int n1 = 0; n1 < N; n1++) {
-			st = new StringTokenizer(br.readLine());
-			for (int n2 = 0; n2 < N; n2++) {
-				int num = Integer.parseInt(st.nextToken());
-				sharkList.add(num);
-				if (num != 0) {
-					sharkNM[num][0] = n1;
-					sharkNM[num][1] = n2;
+			while (L < R) {
+				if (L == n) {
+					L++;
+					continue;
 				}
+
+				if (R == n) {
+					R--;
+					continue;
+				}
+
+				int sum = numbers[L] + numbers[R];
+
+				if (sum == T) {
+					cnt++;
+					break;
+				} else if (sum < T)
+					L++;
+				else
+					R--;
 			}
 		}
 
-		st = new StringTokenizer(br.readLine());
-		for (int m = 1; m <= M; m++)
-			sharkNM[m][2] = Integer.parseInt(st.nextToken());
-
-		for (int m = 1; m <= M; m++) {
-			for (int d1 = 0; d1 < 4; d1++) {
-				st = new StringTokenizer(br.readLine());
-				for (int d2 = 0; d2 < 4; d2++)
-					sharkPQ[m][d1][d2] = Integer.parseInt(st.nextToken());
-			}
-		}
-
-		for (int t = 1; t <= 1000; t++) {
-
-			HashSet<Integer> sharkDelete = new HashSet<>();
-			HashMap<Integer, Integer> sharkCheck = new HashMap<>();
-
-			for (int shark : sharkList) {
-
-			}
-		}
-
-		System.out.println(time);
+		System.out.println(cnt);
 	}
 }
